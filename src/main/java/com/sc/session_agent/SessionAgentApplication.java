2cc6396ed36b46305fc9424dfba2d2852ac76dba
Дмитрий Виноградов <dvinogradov@smart-consulting.ru>
2023-09-20 16:59:40 +0700

    initial commit

---
+++ src/main/java/com/sc/session_agent/SessionAgentApplication.java
@@ -0,0 +1,13 @@
+package com.sc.session_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionAgentApplication.class, args);
+    }
+
+}
